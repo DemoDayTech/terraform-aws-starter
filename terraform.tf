812c6cc6461249d73cb38170ef562655532c52b1
Chris Aguirre <dev@demodaytech.com>
2025-08-19 14:40:59 -0400

    added s3 backend for storing state in s3 bucket

--- terraform.tf
+++ terraform.tf
@@ -2,13 +2,21 @@ terraform {
   /* The following 'cloud' block migrates your state to HCP Terraform. 
   *     You must store your AWS credentials in HCP Terraform -> Workspace -> Variables for this to work. 
   */
-  cloud {
-    organization = "DemoDayTech"
+  # cloud {
+  #   organization = "DemoDayTech"
 
-    workspaces {
-      project = "Learn Terraform"
-      name = "learn-terraform-aws-get-started"
-    }
+  #   workspaces {
+  #     project = "Learn Terraform"
+  #     name = "learn-terraform-aws-get-started"
+  #   }
+  # }
+
+  // The following 'backend' block migrates your state to AWS S3.
+  // Make sure you clear out your .terraform directory if you switch from HCP Terraform to S3 for storing state.
+  backend "s3" {
+    bucket = "terraform-aws-starter"
+    key    = "terraform-state/terraform.tfstate"
+    region = "us-east-2"
   }
 
   required_providers {
